41e60139b8feec1fc3242c79c5dbaccb7aa52a2e
GoldenGoldy92 <153220711+GoldenGoldy92@users.noreply.github.com>
2024-12-22 20:57:55 +0700

    Create wp-config.php

---
+++ var/www/html/wordpress/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', 'wordpress_db');
+define('DB_USER', 'admin');
+define('DB_PASSWORD', 'password123');
+define('DB_HOST', 'my-database-endpoint.amazonaws.com');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+?>
